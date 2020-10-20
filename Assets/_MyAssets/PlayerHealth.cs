1606a2b6ed728e60841b8b894cfe90e40daa417d
Jakub Broda <jakub.broda13@gmail.com>
2020-10-20 21:18:50 +0200

    Add posibility to kill enemies

---
+++ Assets/_MyAssets/PlayerHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : HealthManager
+{
+    public float health = 100f;
+
+    public override void TakeDamage(Vector3 location, Vector3 direction, float damage, Collider bodyPart, GameObject origin)
+    {
+        health -= damage;
+
+        if (health <= 0f)
+            Debug.LogError("DEAD");
+    }
+}
---
+++ Assets/_MyAssets/RaycastWeaponEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BNG;
+
+public class RaycastWeaponEvent : MonoBehaviour
+{
+    private void Awake()
+    {
+        GetComponent<RaycastWeapon>().onRaycastHitEvent.AddListener(OnRaycastHit);
+    }
+
+    private void OnRaycastHit(RaycastHit hit)
+    {
+        if (hit.collider)
+            hit.collider.SendMessageUpwards("HitCallback", new HealthManager.DamageInfo(hit.point, GetComponent<RaycastWeapon>().MuzzlePointTransform.forward, 20f, hit.collider), SendMessageOptions.DontRequireReceiver);
+    }
+}
